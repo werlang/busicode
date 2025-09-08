aa0a4829a51d30ed1b1dde05cf136a3d472563e0
Pablo Werlang <pswerlang@gmail.com>
2025-09-08 19:39:47 +0000

    Preparing for deploy.

--- web/app.js
+++ web/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import mustacheExpress from 'mustache-express';
 
 const app = express();
 app.use(express.json());
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', import.meta.dirname + '/public/');
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || '0.0.0.0';
@@ -9,7 +14,7 @@ const host = process.env.HOST || '0.0.0.0';
 app.use(express.static(import.meta.dirname + '/public/'));
 
 app.get('/', (req, res) => {
-    res.sendFile(import.meta.dirname + '/index.html');
+    res.render('index', { apiurl: process.env.API_URL || 'http://localhost:3000' });
 });
 
 app.listen(port, host, () => {
